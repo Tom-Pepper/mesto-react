2730bafb33e0af8af8466d9377f67daa737bcef8
Artem Kondratyev <walinor.88@gmail.com>
2021-01-14 17:41:04 +0300

    dev: перенесен api в новый проект

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,47 @@
 import React from 'react';
+import api from '../utils/Api';
 
 function Main({isEditAvatarPopupOpen, isEditProfilePopupOpen, isAddPlacePopupOpen}) {
+
+  const [userName, setUserName] = React.useState('');
+  const [userDescription, setUserDescription] = React.useState('');
+  const [userAvatar, setUserAvatar] = React.useState('');
+
+  React.useEffect(() => {
+    Promise.resolve(api.getUserData())
+      .then(res => {
+        setUserName(res.name);
+      })
+      .catch((err) => console.log(err));
+  });
+
+  React.useEffect(() => {
+    Promise.resolve(api.getUserData())
+      .then(res => {
+        setUserDescription(res.about);
+      })
+      .catch((err) => console.log(err));
+  });
+
+  React.useEffect(() => {
+    Promise.resolve(api.getUserData())
+      .then(res => {
+        setUserAvatar(res.avatar);
+      })
+      .catch((err) => console.log(err));
+  });
+
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__wrapper">
           <div className="profile__avatar-wrapper" onClick={isEditAvatarPopupOpen}>
             <button type="button" className="profile__avatar-edit-button"></button>
-            <img className="profile__avatar" src="" alt="Аватар пользователя" />
+            <img className="profile__avatar" src={`${userAvatar}`} alt="Аватар пользователя" />
           </div>
           <div className="profile__info">
-            <h1 className="profile__info-name">Жак-Ив Кусто</h1>
-            <p className="profile__info-job">исследователь, океанолог</p>
+            <h1 className="profile__info-name">{userName}</h1>
+            <p className="profile__info-job">{userDescription}</p>
             <button type="button" className="profile__edit-button" onClick={isEditProfilePopupOpen}></button>
           </div>
         </div>
@@ -21,5 +51,5 @@ function Main({isEditAvatarPopupOpen, isEditProfilePopupOpen, isAddPlacePopupOpe
     </main>
   );
 }
-  
+
 export default Main;
