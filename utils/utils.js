74355d420508df63eddc9dabdc2d9fe8f5d96d66
Artem Kondratyev <walinor.88@gmail.com>
2021-01-28 11:27:26 +0300

    Updates

--- utils/utils.js
+++
@@ -1,9 +0,0 @@
-//Функция для отображения состояния загрузки (UX, лоадер)
-export function buttonLoader(isLoading, popup, loadingText, originalText) {
-  const button = popup.querySelector('.popup__button');
-  if(isLoading) {
-    button.textContent = loadingText;
-  } else {
-    button.textContent = originalText;
-  }
-}
