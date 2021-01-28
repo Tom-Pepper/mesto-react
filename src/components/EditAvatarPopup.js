d5cb9e9b2a9f4ee4bd194c8c05a10953157b64b7
Artem Kondratyev <walinor.88@gmail.com>
2021-01-28 18:13:00 +0300

    refactor: улучшение кода компонента EditAvatarPopup

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,18 +1,10 @@
-import React, { useRef, useState, useEffect, useContext } from 'react';
+import React, { useRef, useEffect } from 'react';
 import PopupWithForm from "./PopupWithForm";
-import CurrentUserContext from "../contexts/CurrentUserContext";
+// import CurrentUserContext from "../contexts/CurrentUserContext";
 
 function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
-  //Подписываемся на контекст
-  const currentUser = useContext(CurrentUserContext);
-
   //Реф для прямого доступа к DOM-элементу инпута и его значению
   const avatarRef = useRef('');
-
-  //Установка аватарки пользователя
-  useEffect(() => {
-    avatarRef.current.value = currentUser.avatar;
-  }, [currentUser])
 
   //Очистка поля ввода ссылки после закрытия
   useEffect(() => {
